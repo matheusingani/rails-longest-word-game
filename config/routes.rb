c190f3c81a06b45f42dead81d14c9b81e72b6de6
matheus ingani <matheusingani@hotmail.com>
2024-10-17 10:06:10 +1100

    update

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+    # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+    get "new", to: "games#new"
+    post "score", to: "games#score"
 end
